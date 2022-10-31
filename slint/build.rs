ec94aabccd01678cf9197a57290ed55bdd2e2e46
Riccardo <riccardo@binaryarm.com>
2022-10-31 18:02:57 +0200

    slint: dark mode

--- slint/build.rs
+++ slint/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     slint_build::compile_with_config(
         "src/mainwindow.slint",
-        slint_build::CompilerConfiguration::new().with_style("fluent".into()),
+        slint_build::CompilerConfiguration::new().with_style("fluent-dark".into()),
     )
     .unwrap();
 }
