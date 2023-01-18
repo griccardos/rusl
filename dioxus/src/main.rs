ab7bc44729b0e7c890f7f71c5deabece2ad23db8
Riccardo <riccardo@binaryarm.com>
2023-01-18 12:53:11 +0200

    colours

--- dioxus/src/main.rs
+++ dioxus/src/main.rs
@@ -1,29 +1,61 @@
 //hide windows console
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 
 use std::{cell::RefCell, sync::mpsc, time::Duration};
 
 use dioxus::prelude::*;
 
+use dioxus_desktop::Config;
 use librusl::{fileinfo::FileInfo, manager::Manager, search::Search};
 
 pub fn main() {
-    dioxus_desktop::launch(app);
+    dioxus_desktop::launch_cfg(
+        app,
+        Config::new().with_custom_index(
+            r#"
+
+            <!DOCTYPE html>
+<html>
+  <head>
+  <link rel="stylesheet" href="./mui.min.css"></link> 
+  <title>rusl</title>
+  <script src: "./mui.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>body { 
+        background-color: black; 
+        color:white;
+    }
+    input::placeholder{
+        color:gray!important
+    }
+    </style>
+  </head>
+  <body>
+    <div id="main"></div>
+  </body>
+</html>
+            "#
+            .into(),
+        ),
+    );
 }
 
 fn app(cx: Scope<()>) -> Element {
     let text_name = use_state(&cx, || "".to_string());
+    let text_contents = use_state(&cx, || "".to_string());
     let text_dir = use_state(&cx, || ".".to_string());
-    let message = use_state(&cx, || "Started".to_string());
+    let message = use_state(&cx, || "".to_string());
     let data = use_state(&cx, || RefCell::new(Vec::<FileInfo>::new()));
     let (s, r) = mpsc::channel();
     let man = use_ref(&cx, || Manager::new(s));
+    let count = use_state(cx, || 0);
 
     //run in background
     // loop, if find then output, else sleep for a while
     use_coroutine(&cx, |_: UnboundedReceiver<()>| {
         let data = data.clone();
         let message = message.clone();
+        let count = count.clone();
         async move {
             loop {
                 match r.try_recv() {
@@ -35,27 +67,39 @@ fn app(cx: Scope<()>) -> Element {
                                 let mut mutable = current.borrow_mut();
                                 mutable.clear();
                                 let found_count = fe.data.len();
-                                 if found_count<1000{
-                                mutable.extend(fe.data);
-                                }else{
-                                    
+                                if found_count < 1000 {
+                                    mutable.extend(fe.data);
+                                } else {
                                     mutable.extend(fe.data.into_iter().take(1000));
-                                    mutable.push(FileInfo { path: format!("...and {} more",found_count-1000), matches: vec![], ext: "".to_string(), name: "".to_string(), is_folder: false });
+                                    mutable.push(FileInfo {
+                                        path: format!("...and {} more", found_count - 1000),
+                                        matches: vec![],
+                                        ext: "".to_string(),
+                                        name: "".to_string(),
+                                        is_folder: false,
+                                    });
                                 };
                                 message.set(format!("Found {} in {:.2}s", found_count, fe.duration.as_secs_f32()));
                             }
                             librusl::manager::SearchResult::InterimResult(ir) => {
+                                count.set(*count.current() + 1);
+
                                 let current = data.current();
                                 let mut mutable = current.borrow_mut();
-                                let mes = format!("Found {}, searching...", mutable.len());
+                                let mes = format!("Found {}, searching...", count.current());
                                 //eprintln!("{mes}");
                                 message.set(mes);
-                               
-                                if mutable.len()<1000{
-                                    
-                                mutable.push(ir);
-                                }else if mutable.len()==1000{
-                                    mutable.push(FileInfo { path: format!("...and others"), matches: vec![], ext: "".to_string(), name: "".to_string(), is_folder: false });
+
+                                if mutable.len() < 1000 {
+                                    mutable.push(ir);
+                                } else if mutable.len() == 1000 {
+                                    mutable.push(FileInfo {
+                                        path: format!("...and others"),
+                                        matches: vec![],
+                                        ext: "".to_string(),
+                                        name: "".to_string(),
+                                        is_folder: false,
+                                    });
                                 }
                             }
                         }
@@ -70,38 +114,118 @@ fn app(cx: Scope<()>) -> Element {
     });
 
     cx.render(rsx!(
-        head {
-            link { rel: "stylesheet", href: "mui.min.css" }
-        }
-        body {
+
             div{
-                class: "py-8 px-6",
-                input{
-                    placeholder:"File name",
+
+
+                //toolbar
+                div{
+                    style:"background-color:#404040;padding:10px;",
+                div{
+                    class:"mui-textfield",
+
+                    input{
+                        style:"color:lightgray;",
                     value:"{text_name}",
+                    placeholder:"Regex file name search e.g. ^mai.*rs$ or r.st or ^best",
                     oninput: move|evt|{ let newval=evt.value.clone(); text_name.set(newval);},
                 }
+                label{
+                    style:"color:white",
+                    "File name"
+                }
+            }
+            div{
+                class:"mui-textfield",
                 input{
-                    placeholder:"Directory",
+                    style:"color:lightgray;",
+                value:"{text_contents}",
+                placeholder:"Regex content search e.g. str.{2}g",
+                oninput: move|evt|{ let newval=evt.value.clone(); text_contents.set(newval);},
+            }
+            label{
+                style:"color:white",
+                "Contents"
+            }
+        }
+            div{
+                class:"mui-textfield",
+                input{
+                    style:"color:lightgray;",
                     value:"{text_dir}",
                     oninput: move|evt|{ let newval=evt.value.clone(); text_dir.set(newval);},
                 },
+                label{
+                    style:"color:white",
+                    "Directory"
+                }
+            }
+            div{
                 "{message}",
+            }
+            div{
             button{
-                class:"inline-block w-full md:w-auto mx-3 my-3 px-2 py-1 font-medium text-white bg-indigo-500 hover:bg-indigo-600 rounded transition duration-200",
+                class:"mui-btn mui-btn--primary mui-btn--raised",
                 onclick:move|_|{
+                    if text_name.is_empty(){
+                        message.set("Nothing to search for".to_string());
+                    }else{
                 message.set("Searching".to_string());
                 man.with_mut(|x|x.search(Search {
                     name_text: text_name.to_string(),
+                    contents_text:text_contents.to_string(),
                     dir: text_dir.to_string(),
                     ..Default::default()
                 }));
-                
-            },"Find"}
-            ul{
-                data.current().borrow().iter().map(|x|rsx!{li{a{"{x.path}"}}})
+            }
+
+
+                },
+                "Find"
+            }
             }
         }
+
+        //results
+            div{
+                style:"padding:10px",
+                data.current().borrow().iter().map(|x|
+                    {
+                        rsx!(
+                    div{
+                        label{
+                            style:if text_contents.is_empty(){""}else{ "color:blue;font-weight:bold"},
+                            "{x.path}"
+                        },
+                    }
+                    for mat in &x.matches{
+                        rsx!(
+                            div{
+                            label{
+                                style:"color:darkgreen;font-weight:bold",
+                                "{mat.line}: "
+                            }
+
+                            label{"{limit_len(&mat.content, 100)}"},
+                        }
+                        )
+                    }
+
+                        )
+
+            }
+            )
+
+
+            }
+
     }
     ))
+}
+
+fn limit_len(s: &str, max: usize) -> &str {
+    match s.char_indices().nth(max) {
+        None => s,
+        Some((i, _)) => &s[..i],
+    }
 }
